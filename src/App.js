75f5db140ec727bd56ac8d2984ec391f4d3ce79b
maheshsarade001 <maheshsarade001@gmail.com>
2022-06-28 11:22:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+import Toast from "./Components/toast/Toast";
+import Button from "./Components/button/Button";
+import { BUTTON_PROPS, TOAST_PROPERTIES } from "./toastProperties";
+import Checkmark from "./Components/checkmark/Checkmark";
+
+const App = () => {
+  const [list, setList] = useState([]);
+  const [position, setPosition] = useState("Select Position");
+  let [checkValue, setCheckValue] = useState(false);
+  const [autoDeleteTime, setAutoDeleteTime] = useState(0);
+
+  const selectPosition = (e) => {
+    setPosition(e.target.value);
+    setList([]);
+  };
+
+  const showToast = (type) => {
+    const toastProperties = TOAST_PROPERTIES.find(
+      (toast) => toast.title.toLowerCase() === type
+    );
+    setList([...list, toastProperties]);
+  };
+
+  const onCheckBoxChange = () => {
+    checkValue = !checkValue;
+    setCheckValue(checkValue);
+    setList([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <p>React Toast Component</p>
+        <div className="select">
+          <Checkmark
+            label="Auto Dismiss"
+            checkValue={checkValue}
+            onCheckBoxChange={onCheckBoxChange}
+          />
+          <br />
+          <input
+            className={`${!checkValue ? "disabled" : ""}`}
+            type="text"
+            name="dismiss"
+            placeholder="Dismiss time Ex: 3000"
+            autoComplete="false"
+            onChange={(e) => setAutoDeleteTime(parseInt(e.target.value, 10))}
+          />
+          <br />
+          <select
+            name="position"
+            value={position}
+            onChange={selectPosition}
+            className="position-select"
+          >
+            <option>Select Position</option>
+            <option value="top-right">Top Right</option>
+            <option value="top-left">Top Left</option>
+            <option value="bottom-left">Bottom Left</option>
+            <option value="bottom-right">Bottom Right</option>
+          </select>
+        </div>
+        <br />
+        <div className="toast-buttons">
+          {BUTTON_PROPS.map((e) => (
+            <Button
+              key={e.id}
+              className={`${
+                position === "Select Position"
+                  ? `${e.className} btn-disable`
+                  : `${e.className}`
+              }`}
+              label={e.label}
+              handleClick={() => showToast(e.type)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <Toast
+        toastList={list}
+        position={position}
+        autoDelete={checkValue}
+        autoDeleteTime={autoDeleteTime}
+      />
+    </>
   );
-}
+};
 
 export default App;
